c737436b9d19cf7f116d0104d7e3b74f0a2e4940
Artyom1363 <artemwulf2015@gmail.com>
2022-09-17 14:43:20 +0300

    added factory boy generating

--- hm02/test.py
+++ hm02/test.py
@@ -1,30 +1,33 @@
 import unittest
 from unittest.mock import patch
 import json_parser
+import json
 from json_parser import parse_json
-
-
-# def keyword_handler(keyword):
-#     print("keyword_handler was called ", f"{keyword=}")
+from faker import Faker
 
 
 class TestParsingJson(unittest.TestCase):
+
+    def setUp(self):
+        fake = Faker()
+        user_dict = {
+            "name": fake.first_name(),
+            "surname": fake.last_name(),
+            "country": fake.country(),
+            "town": "Kovrov"
+        }
+        self.json_str = json.dumps(user_dict)
+
     @patch("json_parser.keyword_handler")
     def test_parsing_json(self, keyword_handler_mock):
         keyword_handler_mock.return_value = None
-        json_str = '{"squadName": "Super hero squad", ' \
-                   '"homeTown": "Metro City", ' \
-                   '"formed": 2016, "secretBase": "Super tower", ' \
-                   '"active": true}'
 
-        parse_json(json_str, ["squadName", 'test'], ["hero", 'make'], json_parser.keyword_handler)
+        parse_json(self.json_str, ["name", "surname", "town"], ["Larisa_", "Kovrov"], json_parser.keyword_handler)
         self.assertTrue(keyword_handler_mock.called)
         self.assertEqual(
-            [unittest.mock.call('hero')],
+            [unittest.mock.call("Kovrov")],
             keyword_handler_mock.mock_calls
         )
-        print(keyword_handler_mock.mock_calls)
-        # print(keyword_handler_mock.called)
 
 
 if __name__ == '__main__':

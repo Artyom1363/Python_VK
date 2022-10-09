a537c80d9ac0f04794e44a8a994b70d988294930
Artyom1363 <artemwulf2015@gmail.com>
2022-10-09 21:13:58 +0300

    wrote lru with tests

---
+++ 05/lru_cache.py
@@ -0,0 +1,99 @@
+from collections import defaultdict
+
+
+class Node:
+    def __init__(self, value, pref=None, nref=None):
+        self.value = value
+        self.nref = nref
+        self.pref = pref
+
+
+class CustomList:
+    def __init__(self) -> None:
+        self.list_start = None
+        self.size = 0
+        self.list_end = None
+
+    def push_front(self, value) -> Node:
+        node = Node(value, None, self.list_start)
+
+        if self.list_start is not None:
+            self.list_start.pref = node
+        else:
+            self.list_end = node
+
+        self.list_start = node
+        self.size += 1
+        return self.list_start
+
+    def move_up(self, node: Node) -> None:
+        if node == self.list_start:
+            return
+
+        if node.nref is not None:
+            node.nref.pref = node.pref
+        else:
+            if node.pref is not None:
+                self.list_end = node.pref
+
+        if node.pref is not None:
+            node.pref.nref = node.nref
+
+        node.pref = None
+        node.nref = self.list_start
+        self.list_start.pref = node
+        self.list_start = node
+
+    def pop_back(self) -> Node:
+        if self.list_end is None:
+            raise Exception("Try to pop from free list")
+
+        deleting = self.list_end
+
+        if self.size == 1:
+            self.list_start = None
+            self.list_end = None
+        else:
+            self.list_end.pref.nref = None
+            self.list_end = self.list_end.pref
+
+        self.size -= 1
+        return deleting
+
+
+class LRUCache:
+    def __init__(self, max_size=1):
+        if max_size < 1:
+            raise Exception("Too small max_size in LRU_cache")
+        self.max_size = max_size
+        self.size = 0
+        self.list_storage = CustomList()
+        self.dict_storage = defaultdict(None)
+
+    def get(self, key):
+        return self[key]
+
+    def set(self, key, value):
+        self[key] = value
+
+    def __setitem__(self, key, value):
+        if key in self.dict_storage:
+            node = self.dict_storage[key]
+            node.value = (key, value)
+            self.list_storage.move_up(node)
+        else:
+            node = self.list_storage.push_front((key, value))
+            self.dict_storage[key] = node
+
+        if self.size == self.max_size:
+            node = self.list_storage.pop_back()
+            del self.dict_storage[node.value[0]]
+        else:
+            self.size += 1
+
+    def __getitem__(self, key):
+        if key not in self.dict_storage:
+            return None
+
+        self.list_storage.move_up(self.dict_storage[key])
+        return self.dict_storage[key].value[1]
---
+++ 05/test_lru.py
@@ -0,0 +1,85 @@
+import unittest
+from lru_cache import CustomList, LRUCache
+
+
+class TestLRUCache(unittest.TestCase):
+    def test_custom_list(self):
+        custom_list = CustomList()
+        one = custom_list.push_front(1)
+        self.assertEqual(custom_list.list_start, custom_list.list_end)
+        self.assertEqual(custom_list.size, 1)
+        self.assertIsNone(custom_list.list_start.nref)
+        self.assertIsNone(custom_list.list_start.pref)
+
+        custom_list.move_up(one)
+        self.assertEqual(custom_list.list_start, custom_list.list_end)
+        self.assertEqual(custom_list.size, 1)
+        self.assertIsNone(custom_list.list_start.nref)
+        self.assertIsNone(custom_list.list_start.pref)
+
+        custom_list.push_front(2)
+        custom_list.push_front(3)
+        self.assertEqual(custom_list.size, 3)
+        self.assertNotEqual(custom_list.list_start, custom_list.list_end)
+        self.assertEqual(custom_list.list_start.value, 3)
+        self.assertEqual(custom_list.list_start.nref.value, 2)
+        self.assertEqual(custom_list.list_end.value, 1)
+        self.assertEqual(custom_list.list_start.nref.nref,
+                         custom_list.list_end)
+
+        custom_list.move_up(one)
+        self.assertEqual(custom_list.list_start.value, 1)
+        self.assertEqual(custom_list.list_start.nref.value, 3)
+        self.assertEqual(custom_list.list_end.value, 2)
+        self.assertEqual(custom_list.list_start.nref.nref,
+                         custom_list.list_end)
+
+        deleted = custom_list.pop_back()
+        self.assertEqual(deleted.value, 2)
+        self.assertEqual(custom_list.list_start.value, 1)
+        self.assertEqual(custom_list.list_start.nref.value, 3)
+        self.assertEqual(custom_list.list_start.nref, custom_list.list_end)
+
+    def test_lru_cache(self):
+        with self.assertRaises(Exception):
+            LRUCache(max_size=0)
+
+        lru = LRUCache(max_size=2)
+        lru['1'] = 1
+        lru['2'] = 2
+        lru['3'] = 3
+        self.assertEqual(lru.size, 2)
+        self.assertEqual(lru.dict_storage['2'].value, ('2', 2))
+
+    def test_from_example(self):
+        cache = LRUCache(2)
+
+        cache.set("k1", "val1")
+        cache.set("k2", "val2")
+
+        self.assertIsNone(cache.get("k3"))
+        self.assertEqual(cache.get("k2"), "val2")
+        self.assertEqual(cache.get("k1"), "val1")
+
+        cache.set("k3", "val3")
+
+        self.assertEqual(cache.get("k3"), "val3")
+        self.assertIsNone(cache.get("k2"))
+        self.assertEqual(cache.get("k1"), "val1")
+
+    def test_from_example_like_dict(self):
+        cache = LRUCache(2)
+
+        cache["k1"] = "val1"
+        cache["k2"] = "val2"
+        self.assertIsNone(cache["k3"])
+        self.assertEqual(cache["k2"], "val2")
+        self.assertEqual(cache["k1"], "val1")
+        cache["k3"] = "val3"
+        self.assertEqual(cache["k3"], "val3")
+        self.assertIsNone(cache["k2"])
+        self.assertEqual(cache["k1"], "val1")
+
+
+if __name__ == '__main__':
+    unittest.main()

fc873f8e46372d932a86d21d0614126b9b2940ea
Artyom1363 <artemwulf2015@gmail.com>
2022-10-31 22:18:21 +0300

    refactoring fetcher (delete semafore everywhere and delete comments)

--- 08/fetcher.py
+++ 08/fetcher.py
@@ -1,10 +1,6 @@
 import asyncio
 import aiohttp
 import aiofiles
-import time
-
-
-counter = 0
 
 
 class Counter:
@@ -18,50 +14,40 @@ class Counter:
 #     print("exception_handler was called")
 
 
-async def fetch(queue, sem, session, stat):
-    # raise Exception("error")
+async def fetch(queue, session, stat):
+
     while True:
         url = await queue.get()
-        # print("yes")
-        global counter
-        counter += 1
 
         try:
             async with session.get(url) as resp:
-                data = await resp.read()
+
                 print("reading url...")
                 if resp.status == 200:
                     stat.good += 1
                 else:
                     stat.bad += 1
-                # assert resp.status == 200
-        # except Exception:
-        #     print(f"\n\n\nException found!!!!\n\n\n")
         finally:
             stat.total += 1
             queue.task_done()
-            # sem.release()
 
 
-async def filling_queue(queue, sem, filename):
+async def filling_queue(queue, filename):
     async with aiofiles.open(filename, "r") as file:
         async for line in file:
-            # await sem.acquire()
             print(line, type(line))
-            # print(type(line))
             await queue.put(line)
 
 
 async def batch_fetch(filename, workers_count=5, queue_size=10):
     tasks = asyncio.Queue(queue_size)
-    sem = asyncio.Semaphore(queue_size)
-    filling_urls = asyncio.create_task(filling_queue(tasks, sem, filename))
+    filling_urls = asyncio.create_task(filling_queue(tasks, filename))
     # await filling_urls
     # time.sleep(1)
     statistics = Counter()
     async with aiohttp.ClientSession() as session:
         workers = [
-            asyncio.create_task(fetch(tasks, sem, session, statistics))
+            asyncio.create_task(fetch(tasks, session, statistics))
             for _ in range(workers_count)
         ]
         await filling_urls
@@ -79,4 +65,4 @@ if __name__ == "__main__":
     # task =
     asyncio.run(batch_fetch("urls.txt"))
     # loop.run_forever()
-    print(counter)
+    # print(counter)

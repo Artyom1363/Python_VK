6605b487a41f85fcb8415f25eb9ce127b4ca2e20
Artyom1363 <artemwulf2015@gmail.com>
2022-11-01 11:22:43 +0300

    added test

--- 08/test.py
+++ 08/test.py
@@ -1,7 +1,7 @@
 import unittest
-# from unittest import mock
-from unittest.mock import AsyncMock, Mock
-from fetcher import fetch
+from unittest import mock
+from unittest.mock import AsyncMock, Mock, patch
+from fetcher import fetch, batch_fetch
 
 
 class TestAsyncFetcher(unittest.IsolatedAsyncioTestCase):
@@ -36,6 +36,51 @@ class TestAsyncFetcher(unittest.IsolatedAsyncioTestCase):
         self.assertEqual(stat.total, 3)
         self.assertEqual(stat.good, 1)
         self.assertEqual(stat.bad, 2)
+
+    @patch('fetcher.print')
+    @patch('aiohttp.ClientSession', new_callable=Mock)
+    async def test_batch_fetch(self, client_session, print_mock):
+
+        good_mock = Mock()
+        resp_sequence = []
+        for status in [200, 200, 200, 200, 200, 500]:
+            resp = Mock()
+            resp.status = status
+            resp_sequence.append(resp)
+
+        good_mock.__aenter__ = AsyncMock(side_effect=resp_sequence)
+        good_mock.__aexit__ = AsyncMock()
+
+        session = Mock()
+        session.get = Mock(return_value=good_mock)
+
+        get_session = Mock()
+        get_session.__aenter__ = AsyncMock(return_value=session)
+        get_session.__aexit__ = AsyncMock()
+
+        client_session.return_value = get_session
+        await batch_fetch("test_urls.txt", workers_count=3, queue_size=5)
+        self.assertEqual(good_mock.__aenter__.call_count, 6)
+        self.assertEqual(good_mock.__aexit__.call_count, 6)
+        self.assertEqual(session.get.call_count, 6)
+        self.assertEqual(session.get.mock_calls, [
+            mock.call.get('https:Python1\n'),
+            mock.call.get('https:Python2\n'),
+            mock.call.get('https:Python3\n'),
+            mock.call.get('https:Python4\n'),
+            mock.call.get('https:Python5\n'),
+            mock.call.get('https:Python6\n')])
+
+        self.assertEqual(print_mock.mock_calls, [
+            mock.call.get('Fetch url... status: 200'),
+            mock.call.get('Fetch url... status: 200'),
+            mock.call.get('Fetch url... status: 200'),
+            mock.call.get('Fetch url... status: 200'),
+            mock.call.get('Fetch url... status: 200'),
+            mock.call.get('Fetch url... status: 500'),
+            mock.call.get('statistics.good=5'),
+            mock.call.get('statistics.total=6')
+        ])
 
 
 if __name__ == '__main__':

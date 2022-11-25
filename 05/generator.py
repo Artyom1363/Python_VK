b8b65099a6c6943f207ba3595f53d12b4131526d
Artyom1363 <artemwulf2015@gmail.com>
2022-11-25 14:51:20 +0300

    Fix generator with test

--- 05/generator.py
+++ 05/generator.py
@@ -3,8 +3,11 @@ def gen_from_file(file_obj, words_to_search=None):
         words_to_search = []
     with open(file_obj, "r", encoding="utf-8") as file:
         for line in file:
+            flag = False
             for word in words_to_search:
                 words_in_line = [word_in_line.strip()
                                  for word_in_line in line.lower().split()]
                 if word.lower() in words_in_line:
-                    yield line
+                    flag = True
+            if flag:
+                yield line
--- 05/test_gen.py
+++ 05/test_gen.py
@@ -5,7 +5,8 @@ from generator import gen_from_file
 class TestGenFromFile(unittest.TestCase):
     def test_gen_from_file(self):
 
-        lines = list(gen_from_file("test_data/file.txt", ["боже", "И"]))
+        lines = list(gen_from_file("test_data/file.txt",
+                                   ["боже", "скука", "И"]))
         self.assertEqual(lines, ["И лучше выдумать не мог.\n",
                                  "Но, боже мой, какая скука\n",
                                  "С больным сидеть и день и ночь,\n",

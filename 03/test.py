782f4eeedd2694276ad965b8372f65521dd197db
Artyom1363 <artemwulf2015@gmail.com>
2022-11-25 13:04:03 +0300

    added elem-by-elem comparison after +, -

--- 03/test.py
+++ 03/test.py
@@ -14,16 +14,28 @@ class TestCustomList(unittest.TestCase):
         self.assertEqual(self.array + array2, CustomList([2, 4, 3]))
         self.assertEqual(len(self.array), 3)
 
-        for a, b in zip(self.array, CustomList([1, 2, 3])):
-            self.assertEqual(a, b)
+        for arr_elem, custom_elem in zip(self.array, CustomList([1, 2, 3])):
+            self.assertEqual(arr_elem, custom_elem)
 
         self.assertEqual(len(array2), 2)
-        for a, b in zip(array2, CustomList([1, 2])):
-            self.assertEqual(a, b)
+        for arr_elem, custom_elem in zip(array2, CustomList([1, 2])):
+            self.assertEqual(arr_elem, custom_elem)
 
         self.assertEqual([1, 2] + self.array, CustomList([2, 4, 3]))
-        self.assertEqual(self.array + [1, 2, 3, 4], CustomList([2, 4, 6, 4]))
-        self.assertEqual([1, 2, 3, 4] + self.array, CustomList([2, 4, 6, 4]))
+
+        test_list = [1, 2, 3, 4]
+        list_addition = self.array + test_list
+        self.assertEqual(list_addition, CustomList([16]))
+        for list_elem, custom_elem in zip(list_addition,
+                                          CustomList([2, 4, 6, 4])):
+            self.assertEqual(list_elem, custom_elem)
+
+        list_addition = test_list + self.array
+        self.assertEqual(list_addition, CustomList([16]))
+        for list_elem, custom_elem in zip(list_addition,
+                                          CustomList([2, 4, 6, 4])):
+            self.assertEqual(list_elem, custom_elem)
+
         self.assertEqual(self.array + [], self.array)
         self.assertEqual([] + self.array, self.array)
 
@@ -32,16 +44,28 @@ class TestCustomList(unittest.TestCase):
         self.assertEqual(self.array - array2, CustomList([0, 0, 3]))
         self.assertEqual(len(self.array), 3)
 
-        for a, b in zip(self.array, CustomList([1, 2, 3])):
-            self.assertEqual(a, b)
+        for arr_elem, custom_elem in zip(self.array, CustomList([1, 2, 3])):
+            self.assertEqual(arr_elem, custom_elem)
 
         self.assertEqual(len(array2), 2)
-        for a, b in zip(array2, CustomList([1, 2])):
-            self.assertEqual(a, b)
+        for arr_elem, custom_elem in zip(array2, CustomList([1, 2])):
+            self.assertEqual(arr_elem, custom_elem)
 
         self.assertEqual([1, 2] - self.array, CustomList([0, 0, -3]))
-        self.assertEqual(self.array - [1, 2, 3, 4], CustomList([0, 0, 0, -4]))
-        self.assertEqual([1, 2, 3, 4] - self.array, CustomList([0, 0, 0, 4]))
+
+        test_list = [1, 2, 3, 4]
+        list_subtraction = self.array - test_list
+        self.assertEqual(list_subtraction, CustomList([-4]))
+        for list_elem, custom_elem in zip(list_subtraction,
+                                          CustomList([0, 0, 0, -4])):
+            self.assertEqual(list_elem, custom_elem)
+
+        list_subtraction = test_list - self.array
+        self.assertEqual(list_subtraction, CustomList([4]))
+        for list_elem, custom_elem in zip(list_subtraction,
+                                          CustomList([0, 0, 0, 4])):
+            self.assertEqual(list_elem, custom_elem)
+
         self.assertEqual(self.array - [], self.array)
         self.assertEqual([] - self.array, CustomList([-1, -2, -3]))
 

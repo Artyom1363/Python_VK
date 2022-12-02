6b52b06029a50e0f182967964d01274a3c7f3f5c
Artyom1363 <artemwulf2015@gmail.com>
2022-12-02 22:52:43 +0300

    updated profile decorator and report (added info about cProfile)

--- 09/profile_decorator.py
+++ 09/profile_decorator.py
@@ -5,22 +5,21 @@ import io
 
 def profile_deco(func):
     class Wrapper:
+        def __init__(self):
+            self.profile = cProfile.Profile()
+            self.stream = io.StringIO()
 
         def print_stat(self):
-            print(self.stat)
+            sortby = "cumulative"
+            stats = pstats.Stats(self.profile, stream=self.stream).sort_stats(sortby)
+            stats.print_stats()
+            stat = self.stream.getvalue()
+            print(stat)
 
         def __call__(self, *args, **kwargs):
-            profile = cProfile.Profile()
-            profile.enable()
-            result = func()
-            profile.disable()
-
-            stream = io.StringIO()
-            sortby = "cumulative"
-            stats = pstats.Stats(profile, stream=stream).sort_stats(sortby)
-            stats.print_stats()
-            self.stat = stream.getvalue()
-
+            self.profile.enable()
+            result = func(*args, **kwargs)
+            self.profile.disable()
             return result
 
     return Wrapper()
